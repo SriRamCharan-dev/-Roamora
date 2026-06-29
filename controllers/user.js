d4864acab8cda6374503547ce5a7886298406cfc
Sri Ram Charan Nandigam <sriramcharannandigam@gmail.com>
2026-06-30 01:59:28 +0530

    feat: implement signup flow, hero section, and related components

--- controllers/user.js
+++ controllers/user.js
@@ -1,12 +1,12 @@
-const mongoose=require("mongoose");
-const User=require("../models/user");
-const passportlocal=require("passport-local-mongoose");
- 
-const userSchema=new Schema ({
-    email:{
-        type:String,
-        required:true,
-        unique:true,
+const mongoose = require("mongoose");
+const User = require("../models/user");
+const passportlocal = require("passport-local-mongoose");
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
     }
 });
 User.plugin(passportlocal);
